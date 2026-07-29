9229c382f00c83366e81087a550935b84cb2382b
itswill00 <anstykx00@gmail.com>
2026-07-29 10:48:15 +0700

    perf(cpu): optimize rate limits and floor frequencies for gaming performance
    Signed-off-by: itswill00 <anstykx00@gmail.com>
    Change-Id: I3afbfad0333be139703340b6869498f561654f1d

--- src/cpu.c
+++ src/cpu.c
@@ -59,14 +59,14 @@ void set_cpu_freqs(int min_lit, int max_lit, int min_big, int max_big, const cha
 void apply_profile(profile_t prof, int tier) {
     switch (prof) {
         case PROFILE_SLEEP:
-            set_cpu_freqs(500000, 1450000, 725000, 1300000, "2000", "500");
+            set_cpu_freqs(500000, 1450000, 725000, 1300000, "2000", "1000");
             sysfs_write("/sys/kernel/fpsgo/fbt/boost_ta", "0");
             sysfs_write("/sys/module/ged/parameters/gpu_bottom_freq", "300000");
             break;
 
         case PROFILE_INTERACTIVE: {
             int big_max = (tier >= 3) ? 2000000 : FREQ_BIG_MAX;
-            set_cpu_freqs(900000, FREQ_LITTLE_MAX, 1100000, big_max, "0", "1000");
+            set_cpu_freqs(900000, FREQ_LITTLE_MAX, 1100000, big_max, "0", "10000");
             sysfs_write("/sys/kernel/fpsgo/fbt/boost_ta", "1");
             sysfs_write("/sys/module/ged/parameters/gpu_bottom_freq", "550000");
             sysfs_write("/sys/module/ged/parameters/g_fb_dvfs_threshold", "12");
@@ -74,25 +74,26 @@ void apply_profile(profile_t prof, int tier) {
         }
 
         case PROFILE_TOUCH:
-            set_cpu_freqs(1200000, FREQ_LITTLE_MAX, 1500000, FREQ_BIG_MAX, "0", "1000");
+            set_cpu_freqs(1200000, FREQ_LITTLE_MAX, 1500000, FREQ_BIG_MAX, "0", "15000");
             sysfs_write("/sys/kernel/fpsgo/fbt/boost_ta", "1");
-            sysfs_write("/sys/module/ged/parameters/gpu_bottom_freq", "700000");
-            sysfs_write("/sys/module/ged/parameters/g_fb_dvfs_threshold", "10");
+            sysfs_write("/sys/module/ged/parameters/gpu_bottom_freq", "650000");
+            sysfs_write("/sys/module/ged/parameters/g_fb_dvfs_threshold", "15");
             break;
 
         case PROFILE_GAMING: {
-            const char *gpu_freq = (tier >= 2) ? "700000" : "800000";
-            set_cpu_freqs(1200000, FREQ_LITTLE_MAX, 1600000, FREQ_BIG_MAX, "0", "2500");
+            const char *gpu_freq = (tier >= 3) ? "700000" : "800000";
+            int big_min = (tier >= 3) ? 1600000 : 1800000;
+            set_cpu_freqs(1400000, FREQ_LITTLE_MAX, big_min, FREQ_BIG_MAX, "0", "30000");
             sysfs_write("/sys/kernel/fpsgo/fbt/boost_ta", "1");
             sysfs_write("/sys/module/ged/parameters/gpu_bottom_freq", gpu_freq);
-            sysfs_write("/sys/module/ged/parameters/g_fb_dvfs_threshold", "20");
+            sysfs_write("/sys/module/ged/parameters/g_fb_dvfs_threshold", "25");
             break;
         }
 
         case PROFILE_THERMAL:
-            set_cpu_freqs(700000, FREQ_LITTLE_MAX, 1000000, 2000000, "0", "1000");
+            set_cpu_freqs(900000, FREQ_LITTLE_MAX, 1200000, 1800000, "2000", "5000");
             sysfs_write("/sys/kernel/fpsgo/fbt/boost_ta", "1");
-            sysfs_write("/sys/module/ged/parameters/gpu_bottom_freq", "450000");
+            sysfs_write("/sys/module/ged/parameters/gpu_bottom_freq", "500000");
             break;
     }
 }
