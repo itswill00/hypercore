120b0d56e52ded4b224b1e179ea4a7e3fa7cc684
itswill00 <anstykx00@gmail.com>
2026-07-29 10:48:21 +0700

    perf(memory): reduce vm swappiness to minimize ZRAM gaming latency
    Signed-off-by: itswill00 <anstykx00@gmail.com>
    Change-Id: I19c2a26b0d9765ec7113c95744c0b64b1da176a0

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,8 +1,8 @@
 #!/system/bin/sh
 # Tanzanite HyperCore post-fs-data initialization script
 
-sysctl -w vm.swappiness=65 >/dev/null 2>&1
+sysctl -w vm.swappiness=15 >/dev/null 2>&1
 sysctl -w vm.dirty_ratio=15 >/dev/null 2>&1
 sysctl -w vm.dirty_background_ratio=5 >/dev/null 2>&1
-sysctl -w vm.vfs_cache_pressure=90 >/dev/null 2>&1
+sysctl -w vm.vfs_cache_pressure=100 >/dev/null 2>&1
 sysctl -w vm.page-cluster=0 >/dev/null 2>&1
