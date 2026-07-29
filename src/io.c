d344e711c841f6cba9ad008542ca86c6f00c8358
itswill00 <anstykx00@gmail.com>
2026-07-29 12:45:47 +0700

    perf(io): increase baseline read_ahead_kb from 128KB to 256KB for faster app switching and DEX loading
    Signed-off-by: itswill00 <anstykx00@gmail.com>
    Change-Id: Id0b2ceb9a513bd17461f5148628f08688de02469

--- src/io.c
+++ src/io.c
@@ -31,7 +31,7 @@ void apply_io_tuning(void) {
             snprintf(p, sizeof(p), "/sys/block/%s/queue/scheduler", ent->d_name);
             sysfs_write(p, "none");
             snprintf(p, sizeof(p), "/sys/block/%s/queue/read_ahead_kb", ent->d_name);
-            sysfs_write(p, "128");
+            sysfs_write(p, "256");
             snprintf(p, sizeof(p), "/sys/block/%s/queue/iostats", ent->d_name);
             sysfs_write(p, "0");
             snprintf(p, sizeof(p), "/sys/block/%s/queue/nr_requests", ent->d_name);
